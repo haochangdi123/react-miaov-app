19accae0646ad518d6ecb155ebe59b14557efc9d
haochangdi123 <haochangdi@163.com>
2020-12-20 18:42:29 +0800

    修改loading
    刚进页面时loading为false，表示未加载数据，显示骨架屏，加载数据时和之后loading为true，隐藏骨架屏，离开页面重置loading为false

--- src/store/reducers/work.js
+++ src/store/reducers/work.js
@@ -1,16 +1,17 @@
+//刚进页面时loading为false，表示未加载数据，显示骨架屏，加载数据时和之后loading为true，隐藏骨架屏，离开页面重置loading为false
 function work(state={
     data: {},
-    loading: true
+    loading: false
 },action) {
     switch (action.type) {
         case "RSET_LOAD":
            return {
                data: {},
-               loading: true
+               loading: false
            }
         case "WORK_LOADOVER":
            return {
-               loading: false,
+               loading: true,
                data: action.data
            }
     }
